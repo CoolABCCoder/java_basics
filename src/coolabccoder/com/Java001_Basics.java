251685b0261ffdf890ef7fd96fa3952a562321f8
CoolABCCoder <coolabccoder@gmail.com>
2017-03-18 20:43:09 +0300

    After push

--- src/coolabccoder/com/Java001_Basics.java
+++ src/coolabccoder/com/Java001_Basics.java
@@ -4,4 +4,10 @@ package coolabccoder.com;
  * Created by Владимир on 18.03.2017.
  */
 public class Java001_Basics {
+    int intVar;
+    long longVar;
+    float floatVar;
+    double doubleVar;
+    boolean boolVar;
+    byte byteVar;
 }
